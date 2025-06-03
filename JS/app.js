be74bf98b08a33f572a26d5f837bfd305e8c9a22
Terrell-Alves <terrell.alves@mscoding.org>
2025-06-03 13:56:29 -0500

    javascript changes
    Added to be able to click each individual square

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,14 @@
 let grid = document.querySelector('.grid');
 let blocks = document.querySelectorAll('.block');
 let randomBlock = Math.floor(Math.random() * blocks.length);
+let battleship = blocks[randomBlock];
 let answer = document.querySelector('.answer');
 let randomSelection = document.querySelector('.randomButton');
 let playButton = document.querySelector('.playButton');
 let againButton = document.querySelector('.againButton');
+let win = false;
+
+console.log(blocks);
 
 grid.style.display = 'none';
 randomSelection.style.display = 'none';
@@ -15,6 +19,26 @@ function playGame () {
     randomSelection.style.display = 'block';
     playButton.style.display = 'none';
     againButton.style.display = ''
+
+    for (let i = 0; i < blocks.length; i++) {
+        console.log(blocks[i]);
+        blocks[i].addEventListener('click', () => {
+            let currentBlock = blocks[i];
+            console.log(currentBlock);
+            if (win == true) {
+                return
+            }
+
+            if (currentBlock == battleship) {
+                currentBlock.style.backgroundColor = 'green';
+                answer.innerText = 'Congratulations! You found the battleship!';
+                win = true;
+            } else {
+                currentBlock.style.backgroundColor = 'red';
+                answer.innerText = 'You missed. Try again.';
+            }
+        })
+    }
 }
 
 playButton.addEventListener('click', playGame);
@@ -23,7 +47,7 @@ againButton.addEventListener('click', () => {
 });
 
 function selectRandom() {
-    let battleship = blocks[randomBlock];
+    console.log(randomBlock);
 
     blocks.forEach((item) => {
         if (item == battleship) {
